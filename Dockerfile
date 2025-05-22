37b3bd011145e88941b9ff7aac77047b75437871
JuanVolkmar <volkamrpadierna@gmail.com>
2025-05-22 18:24:35 -0500

    debug fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.0-slim  # Imagen antigua con vulnerabilidades
+FROM python:3.9.0-slim  # actualizar imagen
 
 WORKDIR /app
 
--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "¡Hola, SecDevOps!"  #SecDevOps
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0') #host='0.0.0.0'
