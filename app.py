d86d863b621b8ce9f9f7e520005c678381806bb2
JuanVolkmar <volkamrpadierna@gmail.com>
2025-06-07 11:33:43 -0500

    prueba 2.0

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola, DevOps!"  #SecDevOps
+    return "¡Hola, SecDevOps!"  #SecDevOps
 
 if __name__ == "__main__":
     app.run(debug=True) #host='127.0.0.1'
